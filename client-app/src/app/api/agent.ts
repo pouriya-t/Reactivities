c962bbcda801730296d5728829e54ea2355cd25b
Pouriya Tajik <pouriya.un@gmail.com>
2020-07-29 11:20:06 +0430

    End of Section 21

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,6 +1,6 @@
 import { IUserFormValues } from "./../models/user";
 import { history } from "../..";
-import { IActivity } from "./../models/activity";
+import { IActivity, IActivitiesEnvelope } from "./../models/activity";
 import axios, { AxiosResponse } from "axios";
 import { toast } from "react-toastify";
 import { IUser } from "../models/user";
@@ -65,7 +65,11 @@ const requests = {
 };
 
 const Activities = {
-  list: (): Promise<IActivity[]> => requests.get("/activities"),
+  list: (params: URLSearchParams): Promise<IActivitiesEnvelope> =>
+    axios
+      .get("/activities", { params: params })
+      .then(sleep(1000))
+      .then(responseBody),
   details: (id: string) => requests.get(`/activities/${id}`),
   create: (activity: IActivity) => requests.post("/activities", activity),
   update: (activity: IActivity) =>
@@ -90,10 +94,15 @@ const Profiles = {
     requests.postForm(`/photos`, photo),
   setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
   deletePhoto: (id: string) => requests.del(`/photos/${id}`),
-  updateProfile: (profile: Partial<IProfile>) => requests.put(`/profiles`,profile),
-  follow: (username: string) => requests.post(`/profiles/${username}/follow`, {}),
+  updateProfile: (profile: Partial<IProfile>) =>
+    requests.put(`/profiles`, profile),
+  follow: (username: string) =>
+    requests.post(`/profiles/${username}/follow`, {}),
   unfollow: (username: string) => requests.del(`/profiles/${username}/follow`),
-  listFollowings: (username: string, predicate: string) => requests.get(`/profiles/${username}/follow?predicate=${predicate}`)
+  listFollowings: (username: string, predicate: string) =>
+    requests.get(`/profiles/${username}/follow?predicate=${predicate}`),
+  listActivities: (username: string, predicate: string) =>
+    requests.get(`/profiles/${username}/activities?predicate=${predicate}`),
 };
 
 export default {
--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,6 +1,6 @@
 import { RootStore } from "./rootStore";
 import { IActivity } from "./../models/activity";
-import { observable, action, computed, runInAction } from "mobx";
+import { observable, action, computed, runInAction, reaction } from "mobx";
 import { SyntheticEvent } from "react";
 import agent from "../api/agent";
 import { history } from "../..";
@@ -12,10 +12,21 @@ import {
   LogLevel,
 } from "@microsoft/signalr";
 
+const LIMIT = 3;
+
 export default class ActivityStore {
   rootStore: RootStore;
   constructor(rootStore: RootStore) {
     this.rootStore = rootStore;
+
+    reaction (
+      () => this.predicate.keys(),
+      () => {
+        this.page = 0;
+        this.activityRegistry.clear();
+        this.loadActivities();
+      }
+    )
   }
 
   @observable activityRegistry = new Map();
@@ -25,6 +36,38 @@ export default class ActivityStore {
   @observable target = "";
   @observable loading = false;
   @observable.ref hubConnection: HubConnection | null = null;
+  @observable activityCount = 0;
+  @observable page = 0;
+  @observable predicate = new Map();
+
+  @action setPredicate = (predicate: string, value: string | Date) => {
+    this.predicate.clear();
+    if (predicate !== "all") {
+      this.predicate.set(predicate, value);
+    }
+  };
+
+  @computed get axiosParams() {
+    const params = new URLSearchParams();
+    params.append("limit", String(LIMIT));
+    params.append("offset", `${this.page ? this.page * LIMIT : 0}`);
+    this.predicate.forEach((value, key) => {
+      if (key === "startDate") {
+        params.append(key, value.toISOString());
+      } else {
+        params.append(key, value);
+      }
+    });
+    return params;
+  }
+
+  @computed get totalPages() {
+    return Math.ceil(this.activityCount / LIMIT);
+  }
+
+  @action setPage = (page: number) => {
+    this.page = page;
+  };
 
   @action createHubConnection = (activityId: string) => {
     this.hubConnection = new HubConnectionBuilder()
@@ -38,8 +81,8 @@ export default class ActivityStore {
       .start()
       .then(() => console.log(this.hubConnection!.state))
       .then(() => {
-        console.log('Attempting to join group');
-        this.hubConnection!.invoke('AddToGroup',activityId);
+        console.log("Attempting to join group");
+        this.hubConnection!.invoke("AddToGroup", activityId);
       })
       .catch((error) => console.log("Error establishing connection: ", error));
 
@@ -49,18 +92,18 @@ export default class ActivityStore {
       });
     });
 
-    this.hubConnection.on('Send',message => {
+    this.hubConnection.on("Send", (message) => {
       toast.info(message);
-    })
+    });
   };
 
   @action stopHubConnection = () => {
-    this.hubConnection!.invoke('RemoveFromGroup', this.activity!.id)
+    this.hubConnection!.invoke("RemoveFromGroup", this.activity!.id)
       .then(() => {
         this.hubConnection!.stop();
       })
-      .then(() => console.log('Connection stopped'))
-      .catch(err => console.log(err))
+      .then(() => console.log("Connection stopped"))
+      .catch((err) => console.log(err));
   };
 
   @action addComment = async (values: any) => {
@@ -96,12 +139,14 @@ export default class ActivityStore {
   @action loadActivities = async () => {
     this.loadingInitial = true;
     try {
-      const activities = await agent.Activities.list();
+      const activitiesEnvelope = await agent.Activities.list(this.axiosParams);
+      const { activities, activityCount } = activitiesEnvelope;
       runInAction("loading activities", () => {
         activities.forEach((activity) => {
           setActivityProps(activity, this.rootStore.userStore.user!);
           this.activityRegistry.set(activity.id, activity);
         });
+        this.activityCount = activityCount;
         this.loadingInitial = false;
       });
     } catch (error) {
